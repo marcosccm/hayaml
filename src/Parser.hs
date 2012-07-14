27f17ee682a665cfcdf20e3cf99df003207b627a
marcosccm <marcosccm@gmail.com>
2012-07-14 16:18:54 -0300

    first pass at parsing yaml key-value pairs

---
+++ src/Parser.hs
@@ -0,0 +1,15 @@
+import Control.Monad
+import Text.ParserCombinators.Parsec
+
+yamlSequence = keyValuePair `endBy` eol
+
+keyValuePair = liftM2 (,) key value
+key          = many nonDelimeterChar
+value        = char ':' >> many nonDelimeterChar
+
+nonDelimeterChar = noneOf ":\n"
+eol = char '\n'
+
+parseYml :: String -> Either ParseError [(String, String)]
+parseYml input = parse yamlSequence "unexpected" input
+
