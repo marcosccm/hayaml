a8bff47b21afaf837cdbfa81f1e81d4ac76624d4
marcosccm <marcosccm@gmail.com>
2012-07-14 21:18:10 -0300

    starting to use yaml models in parsing, ugly code edition

--- src/Hayaml/Parser.hs
+++ src/Hayaml/Parser.hs
@@ -3,15 +3,22 @@ module Hayaml.Parser where
 import Control.Monad
 import Text.ParserCombinators.Parsec
 
+import Hayaml.Model.YamlNode
+
+
 yamlSequence = keyValuePair `endBy` eol
 
-keyValuePair = liftM2 (,) key value
-key          = many nonDelimeterChar
-value        = char ':' >> many nonDelimeterChar
+keyValuePair = YObject `fmap` ((:[]) `fmap` parsedKeyValue) 
+    where parsedKeyValue = liftM2 (,) key value
+
+key = many nonDelimeterChar
+
+value = YString `fmap` parsedValue
+    where parsedValue = char ':' >> many nonDelimeterChar
 
 nonDelimeterChar = noneOf ":\n"
 eol = char '\n'
 
-parseYml :: String -> Either ParseError [(String, String)]
+parseYml :: String -> Either ParseError [YamlNode]
 parseYml input = parse yamlSequence "unexpected" input
 
